0eee6f17b36493be2fdd49fd3f89cac7ca6dbe2c
Alex Dillhoff <ajdillhoff@gmail.com>
2023-04-07 00:00:21 -0500

    Refactored pytorch lightning examples.

--- deep_learning/pl_demo/data_modules/cifar10.py
+++ deep_learning/pl_demo/data_modules/cifar10.py
@@ -5,22 +5,27 @@ import pytorch_lightning as pl
 
 
 class CIFAR10DataModule(pl.LightningDataModule):
-    def __init__(self, data_dir: str="path/to/dir", batch_size: int=128):
+    def __init__(self, data_dir: str="path/to/dir",
+                batch_size: int=128,
+                num_workers: int=8):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
+        self.num_workers = num_workers
 
     def setup(self, stage=None):
         # Assign train/val datasets for use in dataloaders
         if stage == "fit" or stage is None:
             train_transforms = transforms.Compose([
-                transforms.Resize(224),
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
                 transforms.ToTensor(),
                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
             ])
 
             test_transforms = transforms.Compose([
-                transforms.Resize(224),
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
                 transforms.ToTensor(),
                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
             ])
@@ -41,6 +46,8 @@ class CIFAR10DataModule(pl.LightningDataModule):
         # Assign test dataset for use in dataloader(s)
         if stage == "test" or stage is None:
             test_transforms = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
                 transforms.ToTensor(),
                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
             ])
@@ -48,10 +55,25 @@ class CIFAR10DataModule(pl.LightningDataModule):
             self.test_dataset = CIFAR10(self.data_dir, train=False, download=True, transform=test_transforms)
 
     def train_dataloader(self):
-        return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=8, shuffle=True)
+        return torch.utils.data.DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True
+        )
 
     def val_dataloader(self):
-        return torch.utils.data.DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=8, shuffle=False)
+        return torch.utils.data.DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False
+        )
 
     def test_dataloader(self):
-        return torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=8, shuffle=False)
+        return torch.utils.data.DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False
+        )
