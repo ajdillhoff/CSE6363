741225af746a1791d2104b750e1cff97c45cda06
Alex Dillhoff <ajdillhoff@gmail.com>
2023-06-14 09:25:45 -0500

    Added first assignment.

---
+++ summer22_archive/assignments/assignment1/LinearRegression.py
@@ -0,0 +1,72 @@
+class LinearRegression:
+    def __init__(self, batch_size=32, regularization=0, max_epochs=100, patience=3):
+        """Linear Regression using Gradient Descent.
+
+        Parameters:
+        -----------
+        batch_size: int
+            The number of samples per batch.
+        regularization: float
+            The regularization parameter.
+        max_epochs: int
+            The maximum number of epochs.
+        patience: int
+            The number of epochs to wait before stopping if the validation loss
+            does not improve.
+        """
+        self.batch_size = batch_size
+        self.regularization = regularization
+        self.max_epochs = max_epochs
+        self.patience = patience
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y, batch_size=32, regularization=0, max_epochs=100, patience=3):
+        """Fit a linear model.
+
+        Parameters:
+        -----------
+        batch_size: int
+            The number of samples per batch.
+        regularization: float
+            The regularization parameter.
+        max_epochs: int
+            The maximum number of epochs.
+        patience: int
+            The number of epochs to wait before stopping if the validation loss
+            does not improve.
+        """
+        self.batch_size = batch_size
+        self.regularization = regularization
+        self.max_epochs = max_epochs
+        self.patience = patience
+
+        # TODO: Initialize the weights and bias based on the shape of X and y.
+        self.weights = None
+        self.bias = None
+
+        # TODO: Implement the training loop.
+
+    def predict(self, X):
+        """Predict using the linear model.
+
+        Parameters
+        ----------
+        X: numpy.ndarray
+            The input data.
+        """
+        # TODO: Implement the prediction function.
+        pass
+
+    def score(self, X, y):
+        """Evaluate the linear model using the mean squared error.
+
+        Parameters
+        ----------
+        X: numpy.ndarray
+            The input data.
+        y: numpy.ndarray
+            The target data.
+        """
+        # TODO: Implement the scoring function.
+        pass
